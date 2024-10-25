dcba8bfe41d00dbd552b894b921b46671c042902
linderkm <kevin.linder94@gmail.com>
2024-10-24 21:11:09 -0600

    adding comment to request file with instructions

--- spotify_token_request.py
+++ spotify_token_request.py
@@ -1,3 +1,12 @@
+"""
+Notes on use: create a .py file and name it 'spotify_credentials'. In the file, create two variables named "client_id" and "client_secret". Save your Spotify ID and Secret code in these variables, as strings.
+
+Follow instructions here to get the Client ID and Client Secret:
+https://developer.spotify.com/documentation/web-api/tutorials/getting-started
+
+"""
+
+
 import requests
 import json
 from spotify_credentials import client_id, client_secret
