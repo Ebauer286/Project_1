64e6e3e61bc81293fea0bdf118f0ed474e3fa384
mellowjellocello <sarah.pieceofcheese@gmail.com>
2024-11-02 02:46:06 -0600

    finalized csv to spotify ids

--- spotify_credentials.py
+++ spotify_credentials.py
@@ -1,2 +1,2 @@
-client_id = "3e4c9193050949eb863261d6cd9df091"
-client_secret = "e38507132ee2487a831c55b362927e3a"
+client_id = "ea95370748bb4db0a2d1a7cebcad0b8a"
+client_secret = "de35a6053b144498a16601e3184f291e"
